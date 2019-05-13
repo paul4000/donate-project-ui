f70932c5392ef1ca4ab56d5bb7d6449a32fa3ea7
Paulina Koziol <pk.koziol49@gmail.com>
2019-05-14 00:07:19 +0200

    added menu buttons - links for /register and /login

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Login  from './user/Login'
 import {
   Route,
   withRouter,
-  Switch
+  Switch, Link
 } from 'react-router-dom';
 
 import { Layout, notification } from 'antd';
+import Button from "antd/lib/button";
 const { Content } = Layout;
 
 class App extends Component {
@@ -19,6 +20,16 @@ class App extends Component {
         <Content className="app-content">
             <div className="container">
               <Switch>
+                  <Route exact path="/">
+                        <div className="container">
+                            <Button type="primary" htmlType="submit" size="large" className="ant-btn-round">
+                                <Link to="/register"> REGISTER </Link>
+                            </Button>
+                            <Button type="primary" htmlType="submit" size="large" className="ant-btn-round">
+                                <Link to="/login"> LOGIN </Link>
+                            </Button>
+                        </div>
+                  </Route>
                 <Route path="/register" component={Register}/>
                 <Route path="/login" component={Login}/>
               </Switch>
